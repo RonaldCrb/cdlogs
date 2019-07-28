e5332dc400d211a7c5a30b6bb5cc07fbacf3be75
Ron Alonzo <alonzo.ronald@gmail.com>
2019-07-28 18:07:20 +0200

    fixing the thicket

--- src/models/Smartlog.ts
+++ src/models/Smartlog.ts
@@ -1,31 +1,30 @@
 import { Schema, model } from 'mongoose'
 
 const SmartlogSchema = new Schema({
-  date: { type: Date },
-  name: { type: String },
-  location: { type: String },
-  vessel: { type: String },
-  bottom_condition: { type: String },
-  sea_state: { type: String },
-  bottom_temperature: { type: String },
-  bottom_visibility: { type: String },
-  bottom_current: { type: String },
-  diving_method: { type: String },
-  depth: { type: Number },
-  leave_surface: { type: Date },
-  leave_bottom: { type: Date },
-  bottom_time: { type: Number },
-  decompression_completed_at: { type: Date },
-  dive_system: { type: String },
-  breathing_gas: { type: String },
-  work_description: { type: String },
-  equipment_testing_record: { type: String },
-  decompression_schedule: { type: String },
-  decompression_sickness: { type: String },
-  penetration_diving: { type: String },
-  diving_contractor: { type: String },
-  diving_supervisor: { type: String },
-  signature_date: { type: Date },
+  diver_name: { type: String }
+  standby_diver: { type: String }
+  diving_supervisor: { type: String }
+  diving_contractor: { type: String }
+  diver_bailout_pressure: { type: Number }
+  standby_diver_bailout_pressure: { type: Number }
+  breathing_gas: { type: String }
+  dive_system_id: { type: String }
+  sea_state: { type: String }
+  bottom_temperature: { type: String }
+  bottom_visibility: { type: String }
+  bottom_current: { type: String }
+  leave_surface: { type: String }
+  leave_bottom: { type: String }
+  bottom_time: { type: Number }
+  depth: { type: Number }
+  work_description: { type: String }
+  penetration_diving: { type: String }
+  post_dive_assesment: { type: String }
+  dive_method: { type: String }
+  decompression_completed_at: { type: String }
+  location: { type: String }
+  vessel: { type: String }
+  client: { type: String }
 })
 
 export default model('Smartlog', SmartlogSchema)

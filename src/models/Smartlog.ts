d683e8ca31b36c1e6a5859b68c06cf722158a573
Ron Alonzo <alonzo.ronald@gmail.com>
2019-08-17 18:38:22 +0200

    changed smartlogs model + interfaces + endpoints

--- src/models/Smartlog.ts
+++ src/models/Smartlog.ts
@@ -2,24 +2,38 @@ import { Schema, model } from 'mongoose'
 import { ISmartlogModel } from '../types/interfaces'
 
 const SmartlogSchema = new Schema({
-  date: { type: String },
-  diver: { type: String },
-  location: { type: String },
+  // Project context
+  diver_name: { type: String },
+  standby_diver: { type: String },
+  diving_supervisor: { type: String },
+  diving_contractor: { type: String },
   vessel: { type: String },
+  location: { type: String },
+  client: { type: String },
+  date: { type: String },
+  
+  // Dive Profile Details
+  dive_method: { type: String },
+  breathing_gas: { type: String },
+  leave_surface: { type: String },
+  leave_bottom: { type: String },
+  bottom_time: { type: Number },
+  depth: { type: Number },
+  decompression_completed_at: { type: String },
+  post_dive_assesment: { type: String },
+  
+  // Equipment Assurance
+  diver_bailout_pressure: { type: Number },
+  standby_diver_bailout_pressure: { type: Number },
+  dive_system_id: { type: String },
+  work_description: { type: String },
+  penetration_diving: { type: String },
+  
+  // Weather Considerations
   sea_state: { type: String },
   bottom_temperature: { type: String },
   bottom_visibility: { type: String },
   bottom_current: { type: String },
-  depth: { type: Number },
-  leave_surface: { type: String },
-  leave_bottom: { type: String },
-  bottom_time: { type: Number },
-  decompression_completed_at: { type: String },
-  breathing_gas: { type: String },
-  work_description: { type: String },
-  penetration_diving: { type: String },
-  diving_contractor: { type: String },
-  diving_supervisor: { type: String },
 })
 
 export default model<ISmartlogModel>('Smartlog', SmartlogSchema)
--- src/routes/smartlogRoutes.ts
+++ src/routes/smartlogRoutes.ts
@@ -13,7 +13,7 @@ class SmartlogRoutes {
     this.router.get('/', this.smartlogIndex)
     this.router.get('/:id', this.smartlogFind)
     this.router.delete('/:id', this.smartlogDelete)
-    this.router.put('/:id', this.smartlogUpdate)
+    // this.router.put('/:id', this.smartlogUpdate)
     this.router.post('/', this.smartlogCreate)
   }
 
@@ -33,51 +33,15 @@ class SmartlogRoutes {
   }
 
   async smartlogUpdate(req: Request, res: Response): Promise<void> {
-    const newSmartlog: ISmartlog= {
-      date: req.body.date,
-      diver: req.body.diver,
-      location: req.body.location,
-      vessel: req.body.vessel,
-      sea_state: req.body.sea_state,
-      bottom_temperature: req.body.bottom_temperature,
-      bottom_visibility: req.body.bottom_visibility,
-      bottom_current: req.body.bottom_current,
-      depth: req.body.depth,
-      leave_surface: req.body.leave_surface,
-      leave_bottom: req.body.leave_bottom,
-      bottom_time: req.body.bottom_time,
-      decompression_completed_at: req.body.decompression_completed_at,
-      breathing_gas: req.body.breathing_gas,
-      work_description: req.body.work_description,
-      penetration_diving: req.body.penetration_diving,
-      diving_contractor: req.body.diving_contractor,
-      diving_supervisor: req.body.diving_supervisor,
-    }
+    const newSmartlog: ISmartlog = req.body
     const smartlog = await Smartlog.findByIdAndUpdate(req.params.id, newSmartlog)
     res.status(201).json({ data: smartlog })
   }
 
   async smartlogCreate(req: Request, res: Response): Promise<void> {
-    const smartlog: ISmartlog = {
-      date: req.body.date,
-      diver: req.body.diver,
-      location: req.body.location,
-      vessel: req.body.vessel,
-      sea_state: req.body.sea_state,
-      bottom_temperature: req.body.bottom_temperature,
-      bottom_visibility: req.body.bottom_visibility,
-      bottom_current: req.body.bottom_current,
-      depth: req.body.depth,
-      leave_surface: req.body.leave_surface,
-      leave_bottom: req.body.leave_bottom,
-      bottom_time: req.body.bottom_time,
-      decompression_completed_at: req.body.decompression_completed_at,
-      breathing_gas: req.body.breathing_gas,
-      work_description: req.body.work_description,
-      penetration_diving: req.body.penetration_diving,
-      diving_contractor: req.body.diving_contractor,
-      diving_supervisor: req.body.diving_supervisor,
-    }
+    const smartlog: ISmartlog = req.body
+    console.log(req.body)
+    console.log(smartlog)
     const newSmartlog = new Smartlog(smartlog)
     await newSmartlog.save()
     res.status(201).json({ data: newSmartlog })
